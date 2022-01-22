bd4528b9c21e82fc6ec450224b2d65396df350ac
maticamisay <maticamisay99@hotmail.com>
2022-01-22 15:10:30 -0300

    header css terminado

--- components/Header.js
+++ components/Header.js
@@ -5,18 +5,19 @@ import styles from "../styles/Header.module.css";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div>
+      <div className="contenedor">
+        <div className={styles.barra}>
+          <Link href="/">
+            <Image width={400} height={100} src="/img/logo.svg" />
+          </Link>
 
-        <div>
-            <Image width={400} height={100} src='/img/logo.svg'/>
+          <nav className={styles.navegacion}>
+            <Link href="/">Inicio</Link>
+            <Link href="/nosotros">Nosotros</Link>
+            <Link href="/blog">Blog</Link>
+            <Link href="/tienda">Tienda</Link>
+          </nav>
         </div>
-
-        <nav>
-          <Link href="/">Inicio</Link>
-          <Link href="/nosotros">Nosotros</Link>
-          <Link href="/blog">Blog</Link>
-          <Link href="/tienda">Tienda</Link>
-        </nav>
       </div>
     </header>
   );
