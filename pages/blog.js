6c9eb0fb616eb0fc9ed6652672999cea2c144cdf
maticamisay <maticamisay99@hotmail.com>
2022-01-23 17:26:01 -0300

    consumiendo entradas para el blog desde nuestra api

--- pages/blog.js
+++ pages/blog.js
@@ -1,11 +1,36 @@
 import Layout from "../components/Layout";
+import Entrada from "../components/Entrada";
 
-const Blog = () => {
+
+const Blog = ({ entradas }) => {
+
   return (
     <Layout pagina="Blog">
-      <h1 className="heading">Desde Blog</h1>
+      <main className="contenedor">
+        <h2 className="heading">Blog</h2>
+        <div>
+          {entradas.map(entrada => (
+            <Entrada 
+              key={entrada.id}
+              entrada={entrada}
+            />
+          ))}
+        </div>
+      </main>
     </Layout>
   );
 };
+
+export async function getServerSideProps() {
+  const url = "http://localhost:1337/blogs";
+  const respuesta = await fetch(url);
+  const entradas = await respuesta.json();
+
+  return {
+    props: {
+      entradas,
+    },
+  };
+}
 
 export default Blog;
