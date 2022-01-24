2c3bea761dd7ddc9d54248254e6242396027a420
maticamisay <maticamisay99@hotmail.com>
2022-01-23 22:34:53 -0300

    Update getServerSideProps para trabajar con dos peticiones de url

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 import Layout from "../components/Layout";
 import ListadoInicio from "../components/ListadoInicio";
 
-export default function Home({guitarras}) {
+export default function Home({ guitarras, curso }) {
   return (
     <div>
       <Layout pagina="inicio">
         <main className="contenedor">
           <h1 className="heading">Un poco de nuestra coleccion</h1>
-          <ListadoInicio
-            guitarras={guitarras}
-          />
+          <ListadoInicio guitarras={guitarras} />
         </main>
       </Layout>
     </div>
@@ -17,13 +15,23 @@ export default function Home({guitarras}) {
 }
 
 export async function getServerSideProps() {
-  const url = `${process.env.API_URL}/guitarras?_sort=created_at:desc`;
-  const respuesta = await fetch(url);
-  const guitarras = await respuesta.json();
+  const urlGuitarras = `${process.env.API_URL}/guitarras?_sort=created_at:desc`;
+  const urlCursos = `${process.env.API_URL}/cursos`;
+
+  const [resGuitarras, resCursos] = await Promise.all([
+    fetch(urlGuitarras),
+    fetch(urlCursos),
+  ]);
+
+  const [guitarras, curso] = await Promise.all([
+    resGuitarras.json(),
+    resCursos.json(),
+  ]);
 
   return {
     props: {
       guitarras,
+      curso,
     },
   };
 }
