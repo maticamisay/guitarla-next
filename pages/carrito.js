289544c4765b00b0231d03429eb1e8146df4817d
maticamisay <maticamisay99@hotmail.com>
2022-03-08 21:55:33 -0300

    agregamos contenido al carrito

--- pages/carrito.js
+++ pages/carrito.js
@@ -1,12 +1,43 @@
 import Layout from "../components/Layout";
+import Image from "next/image";
 import styles from "../styles/Carrito.module.css";
 
-const Carrito = () => {
+const Carrito = ({ carrito }) => {
   return (
     <Layout pagina={"Carrito de Compras"}>
       <h1 className="heading">Carrito</h1>
       <main className={`${styles.contenido} contenedor`}>
-        <div>1</div>
+        <div className={styles.carrito}>
+          {carrito.length === 0
+            ? "CarritoVacio"
+            : carrito.map((producto) => (
+                <div key={producto.id} className={styles.producto}>
+                  <div>
+                    <Image
+                      layout="responsive"
+                      width={250}
+                      height={480}
+                      src={producto.imagen}
+                      alt={producto.nombre}
+                    />
+                  </div>
+                  <div>
+                    <p className={styles.nombre}>{producto.nombre}</p>
+                    <p className={styles.cantidad}>
+                      Cantidad: {producto.cantidad}
+                    </p>
+                    <p className={styles.precio}>
+                      $<span>{producto.precio}</span>
+                    </p>
+                    <p className={styles.subtotal}>
+                      Subtotal: $
+                      <span>{producto.precio * producto.cantidad}</span>
+                    </p>
+                  </div>
+                  <div> </div>
+                </div>
+              ))}
+        </div>
         <div>2</div>
       </main>
     </Layout>
