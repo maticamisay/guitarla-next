9b762b132c495938f268d869d705e4a8868344a8
maticamisay <maticamisay99@hotmail.com>
2022-03-13 18:06:15 -0300

    actualizar cantidades desde el carrito, y agregamos css

--- pages/_app.js
+++ pages/_app.js
@@ -28,11 +28,22 @@ function MyApp({ Component, pageProps }) {
     }
   };
 
+  const actualizarCantidad = (producto) => {
+    const carritoActualizado = carrito.map((articulo) => {
+      if (articulo.id === producto.id) {
+        articulo.cantidad = producto.cantidad;
+      }
+      return articulo;
+    });
+    setCarrito(carritoActualizado)
+  }
+
   return (
     <Component
       {...pageProps}
       carrito={carrito}
       agregarCarrito={agregarCarrito}
+      actualizarCantidad={actualizarCantidad}
     />
   );
 }
